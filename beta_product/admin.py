7072e41aef414d619b58a7e1ef8e35f66babedca
Saba <narmiabas@gmail.com>
2023-06-11 23:52:33 -0700

    Add beta_product user list admin view

--- beta_product/admin.py
+++ beta_product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from beta_product.models import UserInterest
 
-# Register your models here.
+@admin.register(UserInterest)
+class UserInterestAdmin(admin.ModelAdmin):
+    user = UserInterest.user
+    field = UserInterest.field
+    list_display = ('user', 'field')
+    list_filter = ('user', 'field')
+    search_fields = ('user', 'field')
+    ordering = ('user', 'field')
+    readonly_fields = ('user', 'field')
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'field')
+        }),
+    )
