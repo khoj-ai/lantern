4537096844d1633bfb0e897ab9d58dda81377921
sabaimran <narmiabas@gmail.com>
2023-06-21 18:53:08 -0700

    Add additional APIs for logging users in, out, and validating their credentials
    - Add a view for login, logout, and credentials check using built-in Django functions

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from user_manager.views import UserDetailView, RegisterView
+import user_manager.views as views
 
 urlpatterns = [
-    path("expand/", UserDetailView.as_view(), name="user-details"),
-    path("onboard/", RegisterView.as_view(), name="auth-register"),
+    path("expand/", views.UserDetailView.as_view(), name="user-details"),
+    path("onboard/", views.RegisterView.as_view(), name="auth-register"),
+    path("login/", views.UserLoginView.as_view(), name="auth-login"),
+    path("logout/", views.UserLogoutView.as_view(), name="auth-logout"),
+    path("check/", views.CheckValidCredentials.as_view(), name="auth-check"),
 ]
--- user_manager/views.py
+++ user_manager/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from .serializers import UserSerializer, RegisterSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
 
 
 # Class based view to get user details using Django Token Authentication
@@ -14,6 +16,47 @@ class UserDetailView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class CheckValidCredentials(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserLoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            return Response({}, status=status.HTTP_200_OK)
+        data = request.data
+        username = data.get("email", None)
+        password = data.get("password", None)
+        if username is None or password is None:
+            return Response(
+                {"error": "Please provide both username and password"},
+                status=400,
+            )
+        user = authenticate(username=username, password=password)
+        if not user:
+            return Response(
+                {"error": "Invalid Credentials"}, status=status.HTTP_404_NOT_FOUND
+            )
+        login(request, user)
+        return Response({}, status=status.HTTP_200_OK)
+
+
+class UserLogoutView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        logout(request)
+        return Response({"message": "User Logged Out Successfully"})
 
 
 # Class based view to register new user
