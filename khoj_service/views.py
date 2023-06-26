d9e454dcdf67d6ab9705003dc9ea2c24b0140f94
sabaimran <narmiabas@gmail.com>
2023-06-26 13:59:27 -0700

    [Temporary] Add additional logging information to debug latency

--- khoj_service/views.py
+++ khoj_service/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import redirect
 from rest_framework.response import Response
 from rest_framework import status
 from khoj_service.models import RoutingTable
+import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class DefaultRedirectApiView(APIView):
@@ -13,33 +17,97 @@ class DefaultRedirectApiView(APIView):
     routing_table = RoutingTable.objects.all()
 
     def get(self, request):
+        start_time = time.time()
         user = request.user
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for getting user {user.username}: {latency:.3f}s")
+
+        start_time = time.time()
         request_path = request.get_full_path()
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for getting request path: {latency:.3f}s")
+
+        start_time = time.time()
         service_url = self.routing_table.get_service_url_by_user(user)
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for getting service url: {latency:.3f}s")
+
+        start_time = time.time()
         if service_url is None:
             return Response(
                 {"error": "user does not have a service with Khoj"},
                 status=status.HTTP_404_NOT_FOUND,
             )
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for checking service url: {latency:.3f}s")
 
+        start_time = time.time()
         khoj_response = requests.get(f"{service_url}{request_path}")
-        return HttpResponse(khoj_response)
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for making request to service: {latency:.3f}s")
+
+        start_time = time.time()
+        respond = HttpResponse(khoj_response)
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for responding to request: {latency:.3f}s")
+        return respond
 
     def post(self, request):
+        start_time = time.time()
         user = request.user
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for getting user {user.username}: {latency:.3f}s")
+
+        start_time = time.time()
         request_path = request.get_full_path()
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for getting request path: {latency:.3f}s")
+
+        start_time = time.time()
         service_url = self.routing_table.get_service_url_by_user(user)
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for getting service url: {latency:.3f}s")
+
+        start_time = time.time()
         if service_url is None:
             return Response(
                 {"error": "user does not have a service with Khoj"},
                 status=status.HTTP_404_NOT_FOUND,
             )
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for checking service url: {latency:.3f}s")
 
+        start_time = time.time()
         body = request.data
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for getting request body: {latency:.3f}s")
+
+        start_time = time.time()
         khoj_response = requests.post(
             f"{service_url}{request_path}", json=body, headers=request.headers
         )
-        return HttpResponse(khoj_response)
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for making request to service: {latency:.3f}s")
+
+        start_time = time.time()
+        respond = HttpResponse(khoj_response)
+        end_time = time.time()
+        latency = end_time - start_time
+        logging.info(f"Latency for responding to request: {latency:.3f}s")
+
+        return respond
 
 
 class RedirectToKhojAncilliaryAssets(APIView):
