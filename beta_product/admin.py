30684ab41f69dd48730d0c83e72d63df4b124c78
sabaimran <narmiabas@gmail.com>
2023-11-07 15:18:17 -0800

    Update the admin page to support exporting data

--- beta_product/admin.py
+++ beta_product/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 from beta_product.models import UserInterest
+from import_export.admin import ExportActionMixin
+
+from import_export import resources
+from beta_product.models import UserInterest
 
 
-@admin.register(UserInterest)
-class UserInterestAdmin(admin.ModelAdmin):
+class UserInterestResource(resources.ModelResource):
+    class Meta:
+        model = UserInterest
+        fields = ("id", "user__email")
+
+
+class UserInterestExportActionMixin(ExportActionMixin):
+    def get_export_resource_class(self):
+        """
+        Returns the ResourceClass to use for this view.
+        """
+        return UserInterestResource
+
+
+class UserInterestAdmin(UserInterestExportActionMixin, admin.ModelAdmin):
     user = UserInterest.user
     field = UserInterest.field
     list_display = ("user", "field", "unique_identifier", "waitlist")
     list_filter = ("user", "field")
     search_fields = ("user", "field", "unique_identifier")
     ordering = ("user", "field")
-    readonly_fields = ("user", "field", "unique_identifier")
     fieldsets = (
         (None, {"fields": ("user", "field", "waitlist", "unique_identifier")}),
     )
+
+
+admin.site.register(UserInterest, UserInterestAdmin)
